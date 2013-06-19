a3fc85c019200902986cea8c1dab0088ebb15aa3
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-06-19 12:14:48 -0300

    Create sponsors html and styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @speakers = Newconference::Speaker.featured.order(:name)
+
+    # Sponsors
+    @supports = Newconference::Sponsor.kind('support').order(:name)
+    @organizations = Newconference::Sponsor.kind('organization').order(:name)
+    @sponsors = Newconference::Sponsor.kind('sponsor').order(:name)
   end
 end
