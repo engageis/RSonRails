950b762adbfd60416483e36c3c38203aeb54fb4b
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-07-29 11:11:34 -0500

    Remove region from aws config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,6 @@ begin
       :provider               => 'AWS',               # required
       :aws_access_key_id      => ENV['AWS_KEY'],      # required
       :aws_secret_access_key  => ENV['AWS_SECRET'],   # required
-      :region                 => 'sa-east-1'          # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = 'rsonrails'
   end
